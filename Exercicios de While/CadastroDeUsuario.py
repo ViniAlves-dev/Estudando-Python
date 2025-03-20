e28c762079c97ac1bf1325c9c0afac3706d6928b
Vinicius Barboza <vinibarbosa490@gmail.com>
2025-03-20 10:10:52 -0300

    Update CadastroDeUsuario.py

--- Exercicios de While/CadastroDeUsuario.py
+++ Exercicios de While/CadastroDeUsuario.py
@@ -1,17 +1,22 @@
-nome = input('Nome ').strip().title()
+nome = input('Nome completo: ').strip().title()
 while True:
     if len(nome) <= 3:
         nome = input('Nome inválido. Digite seu nome novamente: ').strip().title()
+        
     elif not nome.replace(' ', '').isalpha():
-        nome = input('Nome inválido. Digite seu nome novamente: ').strip().title()
+        nome = input('Nome inválido. Digite seu nome novamente: ').strip().title() #substiui os espaços e verifica se contem apenas letras no nome
+
+    elif " " not in nome:  # Se não houver espaços no nome
+        nome = input("Parece que você digitou tudo junto. Digite seu nome completo corretamente: ").strip().title()
+
     else:
         break
     
-idade = int(input('Idade '))
+idade = int(input('Idade: '))
 while True:
     try:
         if idade < 0 or idade > 150:
-            idade = int(input('Idade inválida. Digite sua idade novamente:  '))
+            idade = int(input('Idade inválida. Digite sua idade novamente:  '))         
         else:
             break
     except ValueError:
