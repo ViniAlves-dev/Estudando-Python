3300dee7a5a70c10266e7a07103fa3a9425891b6
Vinicius Barboza <vinibarbosa490@gmail.com>
2025-03-20 10:54:35 -0300

    Update CadastroDeUsuario.py

--- Exercicios de While/CadastroDeUsuario.py
+++ Exercicios de While/CadastroDeUsuario.py
@@ -1,26 +1,38 @@
+#Pegando nome do usuario
 nome = input('Nome completo: ').strip().title()
 while True:
     if len(nome) <= 3:
-        nome = input('Nome inválido. Digite seu nome novamente: ').strip().title()
-        
-    elif not nome.replace(' ', '').isalpha():
-        nome = input('Nome inválido. Digite seu nome novamente: ').strip().title() #substiui os espaços e verifica se contem apenas letras no nome
-
+        print('Nome inválido. Digite seu nome novamente: ')
+        continue
+    elif not nome.replace(' ', '').isalpha(): #substiui os espaços e verifica se contem apenas letras no nome
+        print('Nome inválido. Digite seu nome novamente: ')
+        continue
     elif " " not in nome:  # Se não houver espaços no nome
-        nome = input("Parece que você digitou tudo junto. Digite seu nome completo corretamente: ").strip().title()
+        print("Parece que você digitou tudo junto. Digite seu nome completo corretamente: ")
+        continue
 
     else:
         break
     
-idade = int(input('Idade: '))
+#Pegando idade do usuario
 while True:
     try:
+        idade = int(input('Idade ').strip())
         if idade < 0 or idade > 150:
-            idade = int(input('Idade inválida. Digite sua idade novamente:  '))         
+            print('Idade inválida. Digite sua idade novamente:  ')        
         else:
             break
     except ValueError:
         print('Por favor insira um numero valido para a idade ')
+
+
+#Pegando salario do usuario
+
+salario = input('Salario').strip()
+
+
+
+
 
 print('''\nNome: {}
 Idade: {}'''.format(nome, idade))
